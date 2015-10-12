4f965aed08840e544e10ae06df05c29b0d5564ba
Mike <mfo0815@gmail.com>
2015-10-11 20:57:42 -0400

    Checkpoint 25 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,18 +1,19 @@
-<<<<<<< HEAD
 require_relative 'entry.rb'
 require "csv"
-=======
-# #8
+
 require_relative 'entry.rb'
->>>>>>> 198a06d59a35354fc800949e4b9e0262be2e71c7
 
 class AddressBook
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
     end
-    
+
+    def nuke
+        @entries = []
+    end
+
     def add_entry(name, phone_number, email)
         # #9
         index = 0
@@ -26,8 +27,7 @@ class AddressBook
         # #11
         @entries.insert(index, Entry.new(name, phone_number, email))
     end
-    
-<<<<<<< HEAD
+
     def import_from_csv(file_name)
         csv_text = File.read(file_name)
         csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -37,24 +37,17 @@ class AddressBook
         end
 
         end
-    
-=======
->>>>>>> 198a06d59a35354fc800949e4b9e0262be2e71c7
+
+
     def remove_entry(name, phone, email)
         delete_entry = nil
-        
+
         @entries.each do |entry|
-<<<<<<< HEAD
             if name == entry.name && phone == entry.phone_number && email = entry.email
                 delete_entry = entry
             end
         end
-=======
-        if name == entry.name && phone == entry.phone_number && email = entry.email
-            delete_entry = entry
-        end
-    end
->>>>>>> 198a06d59a35354fc800949e4b9e0262be2e71c7
+
         @entries.delete(delete_entry)
     end
 end
